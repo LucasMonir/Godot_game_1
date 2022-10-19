60fad95c113b0001f1f364bebdd929179136e0d8
Lucas <lucasmonir@hotmail.com>
2022-10-18 21:29:15 -0300

    finalizei essa porra

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -21,10 +21,25 @@ func _process(delta):
 		var info = move_and_collide(velocity * delta)
 	if get_tree().get_nodes_in_group("Enemy").size() == 0:
 		get_tree().change_scene("res://Scenes/GameOver.tscn")	
-	
+
 func _unhandled_input(ev):
 	if (ev is InputEventKey and ev.scancode == KEY_SPACE):
 		shoot()	
+		var rand = floor(rand_range(0, 4))
+		if not is_playing():
+			if rand == 1 :
+				$Shot.play()
+			elif rand == 2:
+				$Shot2.play()
+			elif rand == 3:
+				$Shot3.play()
+			elif rand == 4:
+				$Shot4.play()
+			else:
+				$Shot5.play()
+
+func is_playing():
+	return $Shot.playing || $Shot2.playing || $Shot3.playing || $Shot4.playing || $Shot5.playing
 		
 func die():
 	if $Death.playing == false:
